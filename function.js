3d333c3464e9235b5bb013599e373a1424161bff
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com>
2021-01-12 14:08:28 +0100

    function.js was created

--- function.js
+++ function.js
@@ -1,24 +1,3 @@
-const counterPlus = document.querySelector("#counter-plus");
-const counterMinus = document.querySelector("#counter-minus");
-const lowestRow = document.querySelector("#lowest-row");
-const lowRow = document.querySelector("#low-row");
-const highRow = document.querySelector("#high-row");
-const highestRow = document.querySelector("#highest-row");
-const counterItem = document.querySelector(".counter-item")
-
-let counter = 0;
-
-counterPlus.addEventListener("click", (e) => {
-    controlCounterIncrease();
-    color();
-    fullControl();
-})
-counterMinus.addEventListener("click", (e) => {
-    controlCounterDecrease();
-    color();
-    emptyControl();
-})
-
 function color() {
     if (counter === 0) {
         lowestRow.style.background = "white";
@@ -51,31 +30,31 @@ function color() {
 function controlCounterIncrease() {
     if (counter < 100) {
         counterItem.innerHTML = counter = counter + 25;
-        counterItem.innerHTML = "%"+counter;
+        counterItem.innerHTML = "%" + counter;
     }
 }
 
 function controlCounterDecrease() {
     if (counter > 0) {
         counterItem.innerHTML = counter = counter - 25;
-        counterItem.innerHTML = "%"+counter;
+        counterItem.innerHTML = "%" + counter;
     }
 }
 
 function fullControl() {
-    if(counter===100){
+    if (counter === 100) {
         highRow.innerHTML = "Battery Full"
         highRow.style.color = "white"
-    }else{
+    } else {
         highRow.innerHTML = " "
     }
 }
 
 function emptyControl() {
-    if(counter===0){
+    if (counter === 0) {
         highRow.innerHTML = "Battery Empty"
         highRow.style.color = "black"
-    }else{
+    } else {
         highRow.innerHTML = " "
     }
 }
